07b8f571f3e3b58e82b0c611657ee330915660e7
arthuramosantos <arthuramossan@gmail.com>
2024-10-26 13:37:04 -0300

    costumizando 404

--- portifolio/src/app/not-found.tsx
+++ portifolio/src/app/not-found.tsx
@@ -1,9 +1,9 @@
 export default function NotFound() {
   return (
-    <div className="not-found-page flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-gray-950 to-red-800 text-white text-center p-8">
-      <h1 className="text-6xl font-bold mb-4">404</h1>
-      <h2 className="text-3xl font-light mb-6">Página não encontrada...</h2>
-      <p className="text-lg font-light">
+    <div className="not-found-page">
+      <h1>404</h1>
+      <h2>Página não encontrada...</h2>
+      <p>
         O recurso que você estava tentando acessar não está mais disponível.
       </p>
     </div>
